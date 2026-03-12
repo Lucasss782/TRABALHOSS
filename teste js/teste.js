5ccefe1fa70142f65b1294e1c30f06b0df954dc3
Lucasss782 <lucasmeningue@gmail.com>
2026-03-12 20:36:03 -0300

    Add files via upload

---
+++ teste js/teste.js
@@ -0,0 +1,12 @@
+window.alert("teste")
+console.log("panapanapanapana")
+let entrada = prompt("Digite um numero")
+console.log(entrada)
+
+
+let i = 0;
+while(i<10){
+    window.alert(i);
+    i++;
+}
+
